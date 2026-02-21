32141e2816e6a7494d7ec955605f456910e023c0
jnjambrin0 <02jnjam@gmail.com>
2026-02-21 15:29:55 +0100

    Initial Commit

---
+++ PromptImprover/PromptImproverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptImproverApp.swift
+//  PromptImprover
+//
+//  Created by Jaime on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PromptImproverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
